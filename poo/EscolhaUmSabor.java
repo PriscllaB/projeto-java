b891c4a53d86407c311554234dba60fd44e9b45e
Priscilla Barbosa <151968584+PriscllaB@users.noreply.github.com>
2023-12-22 10:24:50 -0300

    Add files via upload

---
+++ poo/EscolhaUmSabor.java
@@ -0,0 +1,53 @@
+package poo;
+
+public class EscolhaUmSabor {
+	
+	
+	private String Morango;
+	private String Chocolate;
+	private String NinhoComNutela;
+	private String DoceDeLeite;
+	private String Abacaxi;
+	public EscolhaUmSabor(String morango, String chocolate, String ninhoComNutela, String doceDeLeite, String abacaxi) {
+		Morango = morango;
+		Chocolate = chocolate;
+		NinhoComNutela = ninhoComNutela;
+		DoceDeLeite = doceDeLeite;
+		Abacaxi = abacaxi;
+	}
+	public String getMorango() {
+		return Morango;
+	}
+	public void setMorango(String morango) {
+		Morango = morango;
+	}
+	public String getChocolate() {
+		return Chocolate;
+	}
+	public void setChocolate(String chocolate) {
+		Chocolate = chocolate;
+	}
+	public String getNinhoComNutela() {
+		return NinhoComNutela;
+	}
+	public void setNinhoComNutela(String ninhoComNutela) {
+		NinhoComNutela = ninhoComNutela;
+	}
+	public String getDoceDeLeite() {
+		return DoceDeLeite;
+	}
+	public void setDoceDeLeite(String doceDeLeite) {
+		DoceDeLeite = doceDeLeite;
+	}
+	public String getAbacaxi() {
+		return Abacaxi;
+	}
+	public void setAbacaxi(String abacaxi) {
+		Abacaxi = abacaxi;
+	}
+	
+	
+	
+
+}
+
